37478b976611a939982b8870ae51504ec472117a
Mateusz Keller <mateusz.keller694@gmail.com>
2019-05-29 21:24:54 +0200

    JAVA - połączenie z bazą

--- JAVA/src/system/Meneger.java
+++ JAVA/src/system/Meneger.java
@@ -1,10 +1,12 @@
 package system;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.sql.*;
 
 import dane.Alarm;
 import dane.Contact;
@@ -16,93 +18,137 @@ public class Meneger {
 	ArrayList<Contact> kontakty = new ArrayList<Contact>();
 	static Duration whenToRemove = Duration.ofDays(10);
 	
+	static String database = "Kontakty.accdb";
+	static Connection conn; static Statement s; static ResultSet rs; 
+
 	public static void main(String[] args) {
-		
-		
+
 		LocalDateTime date = LocalDateTime.now();
-	    System.out.println(date);
-	    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("EEEE dd MMMM HH:mm");
-	    
-	    String fDate = date.format(dateFormat);
-	    System.out.println(fDate);
-	    
-	    LocalDateTime sDate = LocalDateTime.of(2019, 5, 8, 1, 57);
-	    System.out.println(sDate.toString());
-	    
-	    fDate = sDate.format(dateFormat);
-	    System.out.println(fDate);
-	    System.out.println("--------------------");
-	    
-	    LocalTime t = LocalTime.of(0, 30);
-	    Event e1 = new Event("1", sDate, date);
-	    eventy.add(e1);
-	    
-	    Event e2 = new Event("2", sDate, date);
-	    eventy.add(e2);
-	    
-	    Event e3 = e1; //new Event(e1.getTittle(), e1.getStart(), e1.getEnd());
-	    //eventy.remove(e3);
-	    System.out.println(eventy);
-	    
-	    oldEventsGo();
-	    
-	    System.out.println(eventy);
-	 }
-	
-	// 'DANE' bedzie sie wprowadzac w jakims formularzu wiec jesli POLE != '' to ustawiamy to pole
-	
-	//EVENTS
-	public void addEvent(String tittle, LocalDateTime start, LocalDateTime end, 
-							String note, String place, Alarm notification, Contact person)
-	{
+		System.out.println(date);
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("EEEE dd MMMM HH:mm");
+
+		String fDate = date.format(dateFormat);
+		System.out.println(fDate);
+
+		LocalDateTime sDate = LocalDateTime.of(2019, 5, 8, 1, 57);
+		System.out.println(sDate.toString());
+
+		fDate = sDate.format(dateFormat);
+		System.out.println(fDate);
+		System.out.println("--------------------");
+
+		LocalTime t = LocalTime.of(0, 30);
+		Event e1 = new Event("1", sDate, date);
+		eventy.add(e1);
+
+		Event e2 = new Event("2", sDate, date);
+		eventy.add(e2);
+
+		Event e3 = e1; // new Event(e1.getTittle(), e1.getStart(), e1.getEnd());
+		// eventy.remove(e3);
+		System.out.println(eventy);
+
+		oldEventsGo();
+
+		System.out.println(eventy);
+
+		bdShow();
+
+
+
+	}
+
+	// 'DANE' bedzie sie wprowadzac w jakims formularzu wiec jesli POLE != '' to
+	// ustawiamy to pole
+
+	// EVENTS
+	public void addEvent(String tittle, LocalDateTime start, LocalDateTime end, String note, String place,
+			Alarm notification, Contact person) {
 		Event temp = new Event(tittle, start, end);
-		if(note != "") temp.setNote(note);
-		if(place != "") temp.setPlace(place);
-		if(notification != null) temp.setNotification(notification);
-		if(person != null) temp.setPerson(person);
-	
+		if (note != "")
+			temp.setNote(note);
+		if (place != "")
+			temp.setPlace(place);
+		if (notification != null)
+			temp.setNotification(notification);
+		if (person != null)
+			temp.setPerson(person);
+
 		eventy.add(temp);
 	}
-	
-	public void deleteEvent(int numberOfEvent)
-	{
+
+	public void deleteEvent(int numberOfEvent) {
 		eventy.remove(numberOfEvent);
 	}
-	
-	//CONTACTS	
-	public void addContact( String name, String company, String email, String phone)
-	{
-		Contact temp = new Contact(name);	
-		
-		if(company != "") temp.setCompany(company);
-		if(email != "") temp.setEmail(email);
-		if(phone != "") temp.setPhone(phone);
-		
+
+	// CONTACTS
+	public void addContact(String name, String company, String email, String phone) {
+		Contact temp = new Contact(name);
+
+		if (company != "")
+			temp.setCompany(company);
+		if (email != "")
+			temp.setEmail(email);
+		if (phone != "")
+			temp.setPhone(phone);
+
 		kontakty.add(temp);
 	}
-	
-	public void deleteContact(int numberOfContact)
-	{
+
+	public void deleteContact(int numberOfContact) {
 		kontakty.remove(numberOfContact);
 	}
-	
-	//FUNCTIONS
-	static public void oldEventsGo() 
-	{
+
+	// FUNCTIONS
+	static public void oldEventsGo() {
 		Iterator<Event> it = eventy.iterator();
-		
-		while(it.hasNext())
-		{
+
+		while (it.hasNext()) {
 			Event e = it.next();
 			Duration duration = Duration.between(e.getStart(), LocalDateTime.now());
-			
+
 			System.out.println(duration);
-			if(duration.compareTo(whenToRemove) > 0)
+			if (duration.compareTo(whenToRemove) > 0)
 				it.remove();
-			
-				
+
 		}
-	
+
 	}
 	
+	static public void bdShow()
+	{
+		try
+		{
+			conn = DriverManager.getConnection("jdbc:ucanaccess://" + database);
+			s = conn.createStatement();
+			rs = s.executeQuery("SELECT * FROM Kontakty");
+			
+			String ret;
+			while(rs.next())
+			{
+				 ret = 	rs.getString("id") + ". " + 
+						rs.getString("nazwa") + " ";
+				 if(rs.getString("email") != null)
+					 ret += rs.getString("email") + " ";
+				 if(rs.getString("telefon") != null)
+						ret += rs.getString("telefon");
+				 
+				 System.out.println(ret);
+			}
+			
+		}catch (Exception ee) {
+			System.out.println(ee);
+		}
+	}
+	
+	public void bdRead()
+	{
+		
+	}
+	
+	public void bdWrite()
+	{
+		
+	}
+
 }
